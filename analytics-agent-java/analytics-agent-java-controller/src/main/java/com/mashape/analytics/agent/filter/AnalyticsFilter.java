9aa0272f043b643f8f6bbe7366b7665cdc6a869a
shashiranjan84 <shashiranjan84@gmail.com>
2015-02-09 01:44:10 -0800

    used Asynchronous servelet api

--- analytics-agent-java/analytics-agent-java-controller/src/main/java/com/mashape/analytics/agent/filter/AnalyticsFilter.java
+++ analytics-agent-java/analytics-agent-java-controller/src/main/java/com/mashape/analytics/agent/filter/AnalyticsFilter.java
@@ -1,6 +1,9 @@
 package com.mashape.analytics.agent.filter;
 
 import java.io.IOException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,25 +14,35 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 public class AnalyticsFilter implements Filter {
+	
+	private ExecutorService analyticsServicexeExecutor;
+	private int poolSize ;
 
 	@Override
 	public void destroy() {
-		
+		analyticsServicexeExecutor.shutdown();
 	}
 
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res,
 			FilterChain arg2) throws IOException, ServletException {
 		// TODO Auto-generated method stub
-		HttpServletRequest reuest = (HttpServletRequest) req;
-		String paramert = reuest.getQueryString();
-		System.out.print(paramert);
+		HttpServletRequest request = (HttpServletRequest) req;
+		request.startAsync();
+		analyticsServicexeExecutor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				
+				
+			}
+		});
 	}
 
 	@Override
-	public void init(FilterConfig arg0) throws ServletException {
-		// TODO Auto-generated method stub
-		
+	public void init(FilterConfig config) throws ServletException {
+		poolSize = Integer.parseInt(config.getInitParameter("poolSize"));
+		analyticsServicexeExecutor = Executors.newFixedThreadPool(poolSize);
 	}
 
 }
