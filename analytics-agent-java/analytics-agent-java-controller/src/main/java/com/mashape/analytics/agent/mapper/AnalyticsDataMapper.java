9a9e3f3c92cd196f9ed5f9c884af947d01fcbb67
Shashi Ranjan <shashi@mashape.com>
2015-02-09 23:46:58 -0800

    refactored the modules

--- analytics-agent-java/analytics-agent-java-controller/src/main/java/com/mashape/analytics/agent/mapper/AnalyticsDataMapper.java
+++ analytics-agent-java/analytics-agent-java-controller/src/main/java/com/mashape/analytics/agent/mapper/AnalyticsDataMapper.java
@@ -1,13 +1,11 @@
 package com.mashape.analytics.agent.mapper;
 
 import com.google.gson.JsonObject;
-import com.mashape.analytics.agent.modal.Message;
 
 public class AnalyticsDataMapper {
 	
 	public static JsonObject getAnalyticsData(){
 		JsonObject data = new JsonObject();
-		Message message = new Message();
 		
 		return data;
 	}
--- analytics-agent-java/analytics-agent-java-mapper/src/main/java/com/mashape/analytics/agent/mapper/AnalyticsDataALF.java
+++
@@ -1,5 +0,0 @@
-package com.mashape.analytics.agent.mapper;
-
-public class AnalyticsDataALF {
-
-}
